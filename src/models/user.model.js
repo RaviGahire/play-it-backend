d6dca4d2efc1d4835c843009f1afa69589b50319
RaviGahire <ravigahire3@gmail.com>
2026-03-05 22:20:56 +0530

    add jwt token generaters

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
 const userSchema = new Schema({
     username: {
         type: String,
@@ -63,5 +65,23 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
 }
 
+// Jwt tokens
+userSchema.methods.generateAccessToken = function () {
+    // payload 
+    jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+        process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    jwt.sign({_id: this._id,},process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+
+
+}
 
 export const User = mongoose.model("User", userSchema)
