18b056bb2b4801499dc2655ff4a23bc0b80f728e
RaviGahire <ravigahire3@gmail.com>
2026-03-06 01:02:44 +0530

    add multer middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "/public/temp")
+    },
+    filename: function (req, file, cb) {
+        const uniqueName = Date.now() + "_" + Math.round(Math.random() * 1E9)
+        cb(null, file.originalname + "_" + uniqueName)
+    }
+})
+
+
+export const upload = multer({ storage: storage })
