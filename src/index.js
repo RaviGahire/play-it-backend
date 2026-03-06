be68ca7f29c09e4723f06f7931c2edf73ae28f5d
RaviGahire <ravigahire3@gmail.com>
2026-03-06 16:19:44 +0530

    Add controller and router setup

--- src/index.js
+++ src/index.js
@@ -7,5 +7,4 @@ connectDB().then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port: ${process.env.PORT}`)
     })
-})
-    .catch((error) => console.log("MongoDB connection failed!!!", error))
+}).catch((error) => console.log("MongoDB connection failed!!!", error))
