d17aa278d744ac789c384d678fc379b72ccf2ed5
RaviGahire <ravigahire3@gmail.com>
2026-03-06 22:05:53 +0530

    fix unlink file bug

--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -5,9 +5,9 @@ import fs from "fs"
 
 // basic config 
 cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET_KEY,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET_KEY,
     secure: true
 })
 
@@ -15,7 +15,7 @@ cloudinary.config({
 
 // create function to handle file upload
 const uploadOnCloudinary = async (localFilePath) => {
-   
+
     try {
         if (!localFilePath) { throw "File path is required" }
 
@@ -25,8 +25,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         // file has been uplaoded successfully
-        console.log("file is uploaded successfully")
-             // delete temp file after successful upload
+        // console.log("file is uploaded successfully")
+
+        // delete temp file after successful upload
         if (fs.existsSync(localFilePath)) {
             fs.unlinkSync(localFilePath);
         }
@@ -34,16 +35,10 @@ const uploadOnCloudinary = async (localFilePath) => {
         return response
 
     } catch (error) {
-
-       // delete temp file if upload failed
-        if (localFilePath && fs.existsSync(localFilePath)) {
-            fs.unlinkSync(localFilePath);
-        }
-
-        console.error("Cloudinary upload error:", error);
-
+        fs.unlinkSync(localFilePath);
+        console.error("Cloudinary upload error:", error)
         return null;
     }
 }
 
-export {uploadOnCloudinary}
+export { uploadOnCloudinary }
