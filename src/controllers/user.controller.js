d73a28467fd49d98cda5b5d66e56fe569b8c3978
RaviGahire <ravigahire3@gmail.com>
2026-03-07 14:31:19 +0530

    fix bug local image path in create user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,61 +9,62 @@ import fs from "fs"
 export const registerUser = asyncHandler(async (req, res) => {
     // get data from frontend
     const { fullname, username, email, password } = req.body
-
-    // if error in user regisration or some of reason that time also clear the localfile path
     // file validation
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    // for cover image
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
     const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+    try {
 
+        // validation 
+        if ([fullname, username, email, password,].some((field) => field?.trim() === "")) {
+            throw new ApiError(400, "All fields are required")
+        }
 
-    // validation 
-    if ([fullname, username, email, password,].some((field) => field?.trim() === "")) {
-        throw new ApiError(400, "All fields are required")
+        // user already exists
+        const existedUser = await User.findOne({ $or: [{ email }, { username }] })
+        if (existedUser) {
+            throw new ApiError(409, "User email or username already exists")
+        }
+
+        let coverImage;
+        if (coverImageLocalPath) {
+            coverImage = await uploadOnCloudinary(coverImageLocalPath);
+        }
+
+        if (!avatarLocalPath) {
+            throw new ApiError(400, "Avatar file is Required")
+        }
+        const avatar = await uploadOnCloudinary(avatarLocalPath)
+        if (!avatar) {
+            throw new ApiError(400, "Avatar file is Required")
+        }
+        // Create user
+        const user = await User.create({
+            fullname,
+            avatar: avatar.url,
+            coverImage: coverImage?.url || "",
+            email,
+            password,
+            username,
+        })
+
+        const createdUser = await User.findById(user._id).select("-password -refreshToken") // remove pass and token
+        if (!createdUser) {
+            throw new ApiError(500, "User registration failed")
+        }
+        return res.status(201).json(new ApiResponse(200, createdUser, "User register successfully"))
+
+    } catch (error) {
+        if (avatarLocalPath && fs.existsSync(avatarLocalPath)) {
+            fs.unlinkSync(avatarLocalPath);
+        }
+
+        if (coverImageLocalPath && fs.existsSync(coverImageLocalPath)) {
+            fs.unlinkSync(coverImageLocalPath);
+        }
+
+        return res.status(error.statusCode || 500).json(new ApiError(500, null, error.message))
+
     }
 
-
-    // user already exists
-    const existedUser = await User.findOne({ $or: [{ email }, { username }] })
-
-    if (existedUser) {
-        fs.unlinkSync(avatarLocalPath);
-        fs.unlinkSync(coverImageLocalPath);
-        throw new ApiError(409, "User email or username already exists")
-    }
-
-    let coverImage;
-    if (coverImageLocalPath) {
-        coverImage = await uploadOnCloudinary(coverImageLocalPath);
-    }
-
-    if (!avatarLocalPath) {
-
-        throw new ApiError(400, "Avatar file is Required")
-    }
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-
-    if (!avatar) {
-        throw new ApiError(400, "Avatar file is Required")
-    }
-
-    const user = await User.create({
-        fullname,
-        avatar: avatar.url,
-        coverImage: coverImage?.url || "",
-        email,
-        password,
-        username,
-
-    })
-
-    const createdUser = await User.findById(user._id).select("-password -refreshToken")
-
-    if (!createdUser) {
-        fs.unlinkSync(avatarLocalPath);
-        fs.unlinkSync(coverImageLocalPath);
-        throw new ApiError(500, "User registration failed")
-    }
-    return res.status(201).json(new ApiResponse(200, createdUser, "User register successfully"))
 })
 
