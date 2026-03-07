8c057141c7e57202a402a0a82e3ff47c442ee7b0
RaviGahire <ravigahire3@gmail.com>
2026-03-07 13:42:52 +0530

    fix image upload bug

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,39 +3,46 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudnary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
+import fs from "fs"
 
 // user routes
 export const registerUser = asyncHandler(async (req, res) => {
     // get data from frontend
     const { fullname, username, email, password } = req.body
 
+    // if error in user regisration or some of reason that time also clear the localfile path
+    // file validation
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    // for cover image
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+
+
     // validation 
     if ([fullname, username, email, password,].some((field) => field?.trim() === "")) {
         throw new ApiError(400, "All fields are required")
     }
 
+
     // user already exists
     const existedUser = await User.findOne({ $or: [{ email }, { username }] })
 
     if (existedUser) {
+        fs.unlinkSync(avatarLocalPath);
+        fs.unlinkSync(coverImageLocalPath);
         throw new ApiError(409, "User email or username already exists")
     }
 
-    // file validation
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-
-    // for cover image
-    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
     let coverImage;
     if (coverImageLocalPath) {
         coverImage = await uploadOnCloudinary(coverImageLocalPath);
     }
 
     if (!avatarLocalPath) {
+
         throw new ApiError(400, "Avatar file is Required")
     }
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-   
+
     if (!avatar) {
         throw new ApiError(400, "Avatar file is Required")
     }
@@ -53,6 +60,8 @@ export const registerUser = asyncHandler(async (req, res) => {
     const createdUser = await User.findById(user._id).select("-password -refreshToken")
 
     if (!createdUser) {
+        fs.unlinkSync(avatarLocalPath);
+        fs.unlinkSync(coverImageLocalPath);
         throw new ApiError(500, "User registration failed")
     }
     return res.status(201).json(new ApiResponse(200, createdUser, "User register successfully"))
--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -2,7 +2,6 @@ import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 
 
-
 // basic config 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
