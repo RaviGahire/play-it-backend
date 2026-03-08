e96fcb46926f5f5d7569d3d1de25e3d7c67c4eab
RaviGahire <ravigahire3@gmail.com>
2026-03-08 19:53:22 +0530

    add crud routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentUserPassword, getCurrentUser, loginUser, logoutUser, refreshAccessToken, registerUser, updateUserAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJwtToken } from "../middlewares/auth.middleware.js";
 
@@ -14,5 +14,19 @@ router.route("/login").post(loginUser)
 // secured routes
 router.route("/logout").post(verifyJwtToken,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+
+//change user password 
+router.route("/change-password").post(changeCurrentUserPassword)
+
+//get user profile 
+router.route("/profile").get(getCurrentUser)
+
+//update user account details
+router.route("/update").post(updateUserAccountDetails)
+
+//file updation routes
+router.route("/update-avatar").post(updateUserAvatar)
+router.route("/update-cover-image").post(updateUserCoverImage)
 
 export default router
